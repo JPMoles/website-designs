bdf4982efa7df8e694451ada9d0fa85e4fe4f69a
James Moles <jamespmoles@gmail.com>
2021-08-20 16:38:10 -0400

    finished following curves video

--- Day-4/scripts.js
+++ Day-4/scripts.js
@@ -1,1 +1,10 @@
 console.log("Let's get to work!");
+
+const tween = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob1' },
+    { path: '#blob2' },
+    { repeat: 999, duration: 3000, yoyo: true }
+)
+
+tween.start();
